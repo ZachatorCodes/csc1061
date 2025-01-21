014f997b7c0664efd99974e8f1caf58cb5957b93
ZachatorCodes <zachatorcodes@gmail.com>
2025-01-21 15:27:07 -0700

    Format code

--- Lesson1/src/Reverse.java
+++ Lesson1/src/Reverse.java
@@ -6,49 +6,52 @@
 
 /**
  *
- * @author bobgils  Spring 2019
+ * @author bobgils Spring 2019
  */
 public class Reverse {
-    
-    private static void reverse(int[] list) {
-    	// i < j
-    	int reps = 0;
-        for (int i = 0, j = list.length - 1; i < list.length / 2; i++, j--) {
-            // swap list[i] with list[j]
-            int temp = list[i];
-            list[i] = list[j];		
-            list[j] = temp;
-            reps++;
-        }
-        System.out.println(reps);
-    }
-    
-    public static void main(String[] args){
-        int[] list1 = {1, 2, 3, 4, 5, 6,7};
 
-        for (int i : list1)
-            System.out.print(i + " ");
-        System.out.println();
-        
-        reverse(list1);
-        for (int i : list1)
-            System.out.print(i + " ");
-        System.out.println();
-    }
+	private static void reverse(int[] list) {
+		// i < j
+		int reps = 0;
+		for (int i = 0, j = list.length - 1; i < list.length / 2; i++, j--) {
+			// swap list[i] with list[j]
+			int temp = list[i];
+			list[i] = list[j];
+			list[j] = temp;
+			reps++;
+		}
+		System.out.println(reps);
+	}
+
+	public static void main(String[] args) {
+		// Initial integer array
+		int[] list1 = { 1, 2, 3, 4, 5, 6, 7 };
+
+		// For all every integer i inside of list1, print i followed by a space
+		for (int i : list1)
+			System.out.print(i + " ");
+
+		System.out.println(); // Spacer
+
+		reverse(list1); // Call reverse method (pass original list)
+
+		// Loop through list again and print i followed by a space
+		for (int i : list1)
+			System.out.print(i + " ");
+
+		System.out.println(); // Spacer
+	}
 }
 
 /*
-It swaps the elements twice. For example, the first element is swapped with the last element once 
-and then the last element is swapped with the first element another time. In effect, 
-the elements are not swapped. Here is the fix:
-
-int[] list = {1, 2, 3, 5, 4};
-
-for (int i = 0, j = list.length - 1; i < list.length / 2; i++, j--) {
-  // Swap list[i] with list[j]
-  int temp = list[i];
-  list[i] = list[j];
-  list[j] = temp;
-}
-
-*/
+ * It swaps the elements twice. For example, the first element is swapped with
+ * the last element once and then the last element is swapped with the first
+ * element another time. In effect, the elements are not swapped. Here is the
+ * fix:
+ * 
+ * int[] list = {1, 2, 3, 5, 4};
+ * 
+ * for (int i = 0, j = list.length - 1; i < list.length / 2; i++, j--) { // Swap
+ * list[i] with list[j] int temp = list[i]; list[i] = list[j]; list[j] = temp; }
+ * 
+ */
