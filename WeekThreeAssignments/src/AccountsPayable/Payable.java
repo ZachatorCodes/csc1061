319bffdb40534d6ae5df9b5c1be61c36431fd76b
ZachatorCodes <zachatorcodes@gmail.com>
2025-02-08 15:02:07 -0700

    Import files and change packadges

---
+++ WeekThreeAssignments/src/AccountsPayable/Payable.java
@@ -0,0 +1,16 @@
+package AccountsPayable;
+
+// Payable.java
+// Payable interface declaration.
+
+public  class Payable 
+{    
+   double getPaymentAmount() {
+       Class c = getClass();
+       
+       System.out.printf("%s must override method getPaymentAmount()/n", c.getName());
+       
+       return 0;
+   }
+}
+
