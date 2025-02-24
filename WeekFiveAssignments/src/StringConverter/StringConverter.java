e97709fd9246fd18f99a21bc6fc2f2844f46c8ea
ZachatorCodes <zachatorcodes@gmail.com>
2025-02-23 22:14:10 -0700

    Tentative solution

--- WeekFiveAssignments/src/StringConverter/StringConverter.java
+++ WeekFiveAssignments/src/StringConverter/StringConverter.java
@@ -2,6 +2,11 @@ package StringConverter;
 
 public class StringConverter {
 	public static int convert(String numString) {
-		return 0;
+		try {
+			return Integer.parseInt(numString);
+		} catch (Exception e) {
+			throw new NumberFormatException(String.format(
+					"? Invalid numeric string detected starting at %s%nValue of \"%s\" is 0", numString, numString));
+		}
 	}
 }
