9f076dd5b9be09ea41cc0acac02998005d834e4c
ZachatorCodes <zachatorcodes@gmail.com>
2025-02-27 14:23:41 -0700

    Fix assignment using recurrsion

--- WeekFiveAssignments/src/StringConverter/StringConverter.java
+++ WeekFiveAssignments/src/StringConverter/StringConverter.java
@@ -2,11 +2,23 @@ package StringConverter;
 
 public class StringConverter {
 	public static int convert(String numString) {
-		try {
-			return Integer.parseInt(numString);
-		} catch (Exception e) {
-			throw new NumberFormatException(String.format(
-					"? Invalid numeric string detected starting at %s%nValue of \"%s\" is 0", numString, numString));
+		if (numString.charAt(0) == '-') {
+			return (-1 * convertHelper(numString.substring(1), 0));
 		}
+		if (!Character.isDigit(numString.charAt(0))) {
+			throw new NumberFormatException(
+				String.format("? Invalid numeric string detected starting at %s%nValue of \"%s\" is 0", numString, numString)
+			);
+		}
+		return (convertHelper(numString, 0));
+	}
+
+	public static int convertHelper(String numString, int power) {
+		if (numString.length() == 0) {
+			return 0;
+		}
+		int num = Integer.parseInt(String.valueOf(numString.charAt(numString.length() - 1)));
+		int value = num * (int) Math.pow(10, power);
+		return value + convertHelper(numString.substring(0, numString.length() - 1), power + 1);
 	}
 }
