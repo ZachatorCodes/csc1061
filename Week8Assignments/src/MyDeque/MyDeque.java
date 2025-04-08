f173745d2cecab045471093ccff81bd86084cbbb
ZachatorCodes <zachatorcodes@gmail.com>
2025-04-07 22:25:22 -0600

    Update repo

---
+++ Week8Assignments/src/MyDeque/MyDeque.java
@@ -0,0 +1,53 @@
+package MyDeque;
+
+public class MyDeque<E> implements Deque<E> {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addFirst(E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addLast(E e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E removeFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E removeLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
