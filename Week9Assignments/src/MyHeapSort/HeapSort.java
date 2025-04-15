ac35de6377f96cde1a8fee6ce014fb573035b037
ZachatorCodes <zachatorcodes@gmail.com>
2025-04-15 14:55:36 -0600

    Finish assignment

--- Week9Assignments/src/MyHeapSort/HeapSort.java
+++ Week9Assignments/src/MyHeapSort/HeapSort.java
@@ -1,25 +1,36 @@
 package MyHeapSort;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class HeapSort {
-  /** Heap sort method */
-  public static <E extends Comparable> void heapSort(E[] list) {
-    // Create a Heap of integers
-    Heap<E> heap = new Heap<E>();
+	/** Heap sort method */
+	public static <E extends Comparable> void heapSort(E[] list) {
+		// Create a Heap of integers
+		Heap<E> heap = new Heap<E>();
 
-    // Add elements to the heap
-    for (int i = 0; i < list.length; i++)
-      heap.add(list[i]);
+		// Add elements to the heap
+		for (int i = 0; i < list.length; i++)
+			heap.add(list[i]);
 
-    // Remove elements from the heap
-    for (int i = list.length - 1; i >= 0; i--)
-      list[i] = heap.remove();
-  }
-  
-  /** A test method */
-  public static void main(String[] args) {
-    Integer[] list = {-44, -5, -3, 3, 3, 1, -4, 0, 1, 2, 4, 5, 53};
-    heapSort(list);
-    for (int i = 0; i < list.length; i++)
-      System.out.print(list[i] + " ");
-  }
+		heap.printHeap();
+
+		// Remove elements from the heap
+		for (int i = list.length - 1; i >= 0; i--)
+			list[i] = heap.remove();
+	}
+
+	/** A test method */
+	public static void main(String[] args) {
+		Random rndObj = new Random();
+		Integer[] list = new Integer[20];
+
+		for (int i = 0; i < list.length; i++) {
+			list[i] = rndObj.nextInt(-100, 101);
+		}
+
+		System.out.println("Unsorted List: " + Arrays.toString(list));
+		heapSort(list);
+		System.out.println("Sorted List: " + Arrays.toString(list));
+	}
 }
