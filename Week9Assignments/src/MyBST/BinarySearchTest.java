7673ab2cc82e62fd0aa0f8958d138474c05421c0
ZachatorCodes <zachatorcodes@gmail.com>
2025-04-17 13:31:49 -0600

    Finish assignment

--- Week9Assignments/src/MyBST/BinarySearchTest.java
+++ Week9Assignments/src/MyBST/BinarySearchTest.java
@@ -8,34 +8,23 @@ import java.util.Scanner;
 
 public class BinarySearchTest {
 	// perform a binary search on the data
-	public static int binarySearch(int[] data, int key) {
-		int low = 0; // low end of the search area
-		int high = data.length - 1; // high end of the search area
+	public static int binarySearch(int[] data, int low, int high, int key) {
 		int middle = (low + high + 1) / 2; // middle element
 		int location = -1; // return value; -1 if not found
 
-		do // loop to search for element
-		{
-			// print remaining elements of array
-			System.out.print(remainingElements(data, low, high));
+		if (high >= low) {
+			if (data[middle] == key) {
+				return middle;
+			}
+			if (data[middle] > key) {
+				return binarySearch(data, 0, middle - 1, key);
+			} else if (data[middle] < key) {
+				return binarySearch(data, middle + 1, high, key);
+			}
+		}
+		
+		return -1;
 
-			// output spaces for alignment
-			for (int i = 0; i < middle; i++)
-				System.out.print("   ");
-			System.out.println(" * "); // indicate current middle
-
-			// if the element is found at the middle
-			if (key == data[middle])
-				location = middle; // location is the current middle
-			else if (key < data[middle]) // middle element is too high
-				high = middle - 1; // eliminate the higher half
-			else // middle element is too low
-				low = middle + 1; // eliminate the lower half
-
-			middle = (low + high + 1) / 2; // recalculate the middle
-		} while ((low <= high) && (location == -1));
-
-		return location; // return location of search key
 	} // end method binarySearch
 
 	// method to output certain values in array
@@ -72,7 +61,7 @@ public class BinarySearchTest {
 		// repeatedly input an integer; -1 terminates the program
 		while (searchInt != -1) {
 			// perform search
-			int location = binarySearch(data, searchInt);
+			int location = binarySearch(data, 0, data.length - 1, searchInt);
 
 			if (location == -1) // not found
 				System.out.printf("%d was not found%n%n", searchInt);
